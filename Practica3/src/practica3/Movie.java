ed70b72c4a0fb9b4144cf4898b3f475536338dee
Hector Dominguez Diego <h.dominguezd@alumnos.urjc.es>
2018-05-15 12:09:13 +0200

    without Json fields

---
+++ Practica3/src/practica3/Movie.java
@@ -0,0 +1,146 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package practica3;
+
+import java.util.Date;
+import java.util.Map;
+import org.json.simple.JSONObject;
+
+/**
+ *
+ * @author h.dominguezd
+ */
+public class Movie {
+    private int budget;
+    private JSONObject genres;
+    private JSONObject keywords;  
+    private String originalLang;
+    private String originalTitle;
+    private float popularity;
+    private JSONObject productionCompany;
+    private String[] originalDate;
+    private long revenue;
+    private String status;
+    private float voteAverage;
+    private int voteCount;
+
+    public Movie(int budget, JSONObject genres, JSONObject keywords, String originalLang, String originalTitle, float popularity, JSONObject productionCompany, String[] originalDate, long revenue, String status, float voteAverage, int voteCount) {
+        this.budget = budget;
+        this.genres = genres;
+        this.keywords = keywords;
+        this.originalLang = originalLang;
+        this.originalTitle = originalTitle;
+        this.popularity = popularity;
+        this.productionCompany = productionCompany;
+        this.originalDate = originalDate;
+        this.revenue = revenue;
+        this.status = status;
+        this.voteAverage = voteAverage;
+        this.voteCount = voteCount;
+    }
+
+    Movie() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public int getBudget() {
+        return budget;
+    }
+
+    public void setBudget(int budget) {
+        this.budget = budget;
+    }
+
+    public JSONObject getGenres() {
+        return genres;
+    }
+
+    public void setGenres(JSONObject genres) {
+        this.genres = genres;
+    }
+
+    public JSONObject getKeywords() {
+        return keywords;
+    }
+
+    public void setKeywords(JSONObject keywords) {
+        this.keywords = keywords;
+    }
+
+    public String getOriginalLang() {
+        return originalLang;
+    }
+
+    public void setOriginalLang(String originalLang) {
+        this.originalLang = originalLang;
+    }
+
+    public String getOriginalTitle() {
+        return originalTitle;
+    }
+
+    public void setOriginalTitle(String originalTitle) {
+        this.originalTitle = originalTitle;
+    }
+
+    public float getPopularity() {
+        return popularity;
+    }
+
+    public void setPopularity(float popularity) {
+        this.popularity = popularity;
+    }
+
+    public JSONObject getProductionCompany() {
+        return productionCompany;
+    }
+
+    public void setProductionCompany(JSONObject productionCompany) {
+        this.productionCompany = productionCompany;
+    }
+
+    public String[] getOriginalDate() {
+        return originalDate;
+    }
+
+    public void setOriginalDate(String[] originalDate) {
+        this.originalDate = originalDate;
+    }
+
+    public long getRevenue() {
+        return revenue;
+    }
+
+    public void setRevenue(long revenue) {
+        this.revenue = revenue;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public float getVoteAverage() {
+        return voteAverage;
+    }
+
+    public void setVoteAverage(float voteAverage) {
+        this.voteAverage = voteAverage;
+    }
+
+    public int getVoteCount() {
+        return voteCount;
+    }
+
+    public void setVoteCount(int voteCount) {
+        this.voteCount = voteCount;
+    }
+    
+    
+}
--- Practica3/src/practica3/Practica3.java
+++ Practica3/src/practica3/Practica3.java
@@ -5,17 +5,121 @@
  */
 package practica3;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.math.BigInteger;
+import java.util.*;
+import java.util.stream.Collectors;
+import org.json.simple.JSONObject;
+import org.json.simple.JSONValue;
 /**
  *
  * @author h.dominguezd
  */
 public class Practica3 {
 
+    private List<Movie> movies;
+    
+    public Practica3(String path){
+        readDatabase(path);
+    }
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Practica3 practica = new Practica3("./movies_db.csv");
+        practica.printAllMovies();
+        System.out.println("---------------------------------");
+        practica.budgetBetween(0, 100000000);
+        System.out.println("---------------------------------");
+        practica.languageAndPopularity("en", 150).forEach((m) -> {
+            System.out.println(m.getOriginalTitle());
+        });
+        System.out.println("---------------------------------");
+        System.out.println(practica.totalYearRevenue(2012));
+        System.out.println("---------------------------------");
+        System.out.println(practica.totalVotesBetweenAverages(5, 8));
+        
+    }
+
+
+    private void readDatabase(String path) {
+        try (BufferedReader bf = new BufferedReader(new FileReader(path))) {
+            movies = bf.lines()
+                    .skip(1)
+                    .map(line -> line.split(";"))
+                    .filter(tokens -> tokens.length == 12)
+                    .map(tokens ->  {
+                        try {
+                            int budget = Integer.parseInt(tokens[0]);
+                            JSONObject genres = (JSONObject)  JSONValue.parse(tokens[1]);
+                            JSONObject keywords = (JSONObject) JSONValue.parse(tokens[2]);
+                            String originalLang = tokens[3];
+                            String originalTitle = tokens[4];
+                            float popularity = Float.parseFloat(tokens[5]);
+                            JSONObject productionCompanies = (JSONObject) JSONValue.parse(tokens[6]);
+                            
+                            String[] date = tokens[7].split("/");
+                            long revenue = Long.parseLong(tokens[8]);
+                            String status = tokens[9];
+                            float voteAverage = Float.parseFloat(tokens[10]);
+                            int voteCount = Integer.parseInt(tokens[11]);
+                            return new Movie(budget, genres, keywords, originalLang, originalTitle, popularity, productionCompanies, date, revenue, status, voteAverage, voteCount);
+                        } catch (Exception e) {
+                            return null;
+                        }
+                    })
+                    .filter(Objects::nonNull)
+                    .collect(Collectors.toList());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    
+    public void budgetBetween(int min, int max) {
+        movies.parallelStream()
+                .filter(m -> m.getBudget() >= min && m.getBudget() <= max)
+                .map(m -> m.getOriginalTitle())
+                .forEach(System.out::println);
+    }
+    
+    public void printAllMovies(){
+        movies.parallelStream().forEach(System.out::println);
+    }
+    
+    public void genresSet(){
+        
+    }
+    
+    public void keywordMaxRevenue(){
+        
+    }
+    
+    public List<Movie> languageAndPopularity(String lang, float popularity){
+        return movies.parallelStream()
+                .filter(m -> m.getOriginalLang().equals(lang) && m.getPopularity() >= popularity)
+                .collect(Collectors.toList());
+    }
+    
+    public BigInteger totalYearRevenue(int year){
+        return movies.parallelStream()
+            .filter(m -> Integer.parseInt(m.getOriginalDate()[2]) == year)
+            .map(m -> BigInteger.valueOf(m.getRevenue()))
+            .reduce(BigInteger::add)
+            .get();   
+    }
+    public BigInteger totalVotesBetweenAverages(float min, float max){
+        return movies.parallelStream()
+                .filter(m -> m.getVoteAverage() >= min && m.getVoteAverage() <= max)
+                .map(m -> BigInteger.valueOf(m.getVoteCount()))
+                .reduce(BigInteger::add)
+                .get();
+    }
+    
+    public Map<String, String> companiesMap (){
+        return null;   
     }
     
 }
